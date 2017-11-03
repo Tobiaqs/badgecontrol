b65031514d5c8abde186c695802f20d718190be8
Tobias Sijtsma <tobias@tobiass.nl>
2017-11-03 16:17:57 +0100

    done

--- client/badgecontrol.py
+++ client/badgecontrol.py
@@ -1,9 +1,42 @@
 import ugfx, appglue, wifi, usocket, badge, time
 
-badge.nvs_set_str("badge", "wifi.ssid", "hi")
-badge.nvs_set_str("badge", "wifi.password", "hellohello")
-
 font = "Roboto_Regular12"
+
+def reset():
+	ugfx.input_init()
+	ugfx.input_attach(ugfx.BTN_START, lambda pressed: appglue.home() if pressed else None)
+	ugfx.input_attach(ugfx.BTN_A, lambda pressed: bind_to_client() if pressed else None)
+
+	ugfx.clear(ugfx.BLACK)
+	ugfx.flush()
+	ugfx.clear(ugfx.WHITE)
+	ugfx.string_box(0, 0, 296, 128, "Press START again to exit.\nPress A to reconnect", font, ugfx.BLACK, ugfx.justifyCenter)
+	ugfx.flush()
+
+def bind_to_client():
+	ugfx.clear(ugfx.BLACK)
+	ugfx.flush()
+	ugfx.clear(ugfx.WHITE)
+	ugfx.string_box(0, 0, 296, 128, "My IP: " + wifi.sta_if.ifconfig()[0] + "\nReady to receive connection", font, ugfx.BLACK, ugfx.justifyCenter)
+	ugfx.flush()
+
+	connection, client_address = socket.accept()
+
+	ugfx.clear(ugfx.BLACK)
+	ugfx.flush()
+	ugfx.clear(ugfx.WHITE)
+	ugfx.string_box(0, 0, 296, 128, "My IP: " + wifi.sta_if.ifconfig()[0] + "\nYour IP: " + client_address[0] +"\nConnection successful\nPress START to disconnect", font, ugfx.BLACK, ugfx.justifyCenter)
+	ugfx.flush()
+
+	ugfx.input_init()
+	ugfx.input_attach(ugfx.BTN_START, lambda pressed: reset() if pressed else None)
+	ugfx.input_attach(ugfx.JOY_LEFT, lambda pressed: connection.sendall(b"left") if pressed else None)
+	ugfx.input_attach(ugfx.JOY_UP, lambda pressed: connection.sendall(b"up") if pressed else None)
+	ugfx.input_attach(ugfx.JOY_RIGHT, lambda pressed: connection.sendall(b"right") if pressed else None)
+	ugfx.input_attach(ugfx.JOY_DOWN, lambda pressed: connection.sendall(b"down") if pressed else None)
+	ugfx.input_attach(ugfx.BTN_A, lambda pressed: connection.sendall(b"A") if pressed else None)
+	ugfx.input_attach(ugfx.BTN_B, lambda pressed: connection.sendall(b"B") if pressed else None)
+	ugfx.input_attach(ugfx.BTN_SELECT, lambda pressed: connection.sendall(b"SELECT") if pressed else None)
 
 ugfx.init()
 ugfx.input_init()
@@ -21,44 +54,13 @@ ugfx.flush()
 
 #slower than default to save energy
 while not wifi.sta_if.isconnected():
-  time.sleep(0.5)
+	time.sleep(0.5)
+
+socket = usocket.socket()
+socket.bind(('0.0.0.0', 1337))
+socket.listen(1000)
 
 ugfx.clear(ugfx.WHITE)
 ugfx.flush()
-ugfx.clear(ugfx.BLACK)
-ugfx.flush()
-ugfx.clear(ugfx.WHITE)
-ugfx.string_box(0, 0, 296, 128, "My IP: " + wifi.sta_if.ifconfig()[0] + "\nLet me know your IP", font, ugfx.BLACK, ugfx.justifyCenter)
-ugfx.flush()
 
-s = usocket.socket()
-ai = usocket.getaddrinfo("0.0.0.0", 80)
-addr = ai[0][-1]
-s.bind(addr)
-s.listen(5)
-
-ip = s.accept()[1]
-
-s.close()
-
-s = usocket.socket()
-addr = usocket.getaddrinfo(ip, 1337)[0][-1]
-s.connect(addr)
-
-
-ugfx.clear(ugfx.BLACK)
-ugfx.flush()
-ugfx.clear(ugfx.WHITE)
-ugfx.string_box(0, 0, 296, 128, "My IP: " + wifi.sta_if.ifconfig()[0] + "\nYour IP: " + ip, font, ugfx.BLACK, ugfx.justifyCenter)
-ugfx.flush()
-
-def send(action):
-  s.send(action)
-
-ugfx.input_attach(ugfx.JOY_LEFT, lambda pressed: send("left") if pressed else None)
-ugfx.input_attach(ugfx.JOY_UP, lambda pressed: send("up") if pressed else None)
-ugfx.input_attach(ugfx.JOY_RIGHT, lambda pressed: send("right") if pressed else None)
-ugfx.input_attach(ugfx.JOY_DOWN, lambda pressed: send("down") if pressed else None)
-ugfx.input_attach(ugfx.BTN_A, lambda pressed: send("A") if pressed else None)
-ugfx.input_attach(ugfx.BTN_B, lambda pressed: send("B") if pressed else None)
-ugfx.input_attach(ugfx.BTN_SELECT, lambda pressed: send("SELECT") if pressed else None)
+bind_to_client()
--- server/index.js
+++ server/index.js
@@ -2,22 +2,25 @@ const robot = require('robotjs');
 
 const net = require('net');
 
-const badgeIP = '192.168.1.1';
-
-const server = net.createServer((socket) => {
-	socket.on('data', (action) => {
-		console.log(action);
-
-		if (action === 'left' || action === 'up' || action === 'right' || action === 'down') {
-			robot.keyTap(action);
-		} else if (action === 'A') {
-			robot.keyTap('enter');
-		} else if (action === 'B') {
-			console.log('B');
-		} else if (action === 'SELECT') {
-			console.log('SELECT');
-		}
-	});
+let client = new net.Socket();
+client.connect(1337, '192.168.1.104', () => {
+	console.log('connected to badge!');
 });
 
-server.listen(1337, badgeIP);
+client.on('data', (buffer) => {
+	const action = buffer.toString();
+	if (action === 'left' || action === 'up' || action === 'right' || action === 'down') {
+		robot.keyTap(action);
+	} else if (action === 'A') {
+		robot.keyTap('enter');
+	} else if (action === 'B') {
+		console.log('B');
+	} else if (action === 'SELECT') {
+		console.log('SELECT');
+	}
+});
+
+client.on('close', () => {
+	console.log('socket closed');
+});
+
